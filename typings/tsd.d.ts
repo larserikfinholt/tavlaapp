e34323b8eae4bdb72e314b519f3d45999ad54cbb
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2015-05-14 22:51:27 +0200

    Added typescript support

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="moment/moment.d.ts" />
+/// <reference path="ionic/ionic.d.ts" />
+/// <reference path="azure-mobile-services-client/AzureMobileServicesClient.d.ts" />
